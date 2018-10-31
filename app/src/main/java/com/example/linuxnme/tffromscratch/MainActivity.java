020590b9a2d872085c8b8b996c45d445375f3317
Unix&Me <unixnme@gmail.com>
2018-10-31 21:50:56 +0900

    Tensorflow library loaded

--- app/src/main/java/com/example/linuxnme/tffromscratch/MainActivity.java
+++ app/src/main/java/com/example/linuxnme/tffromscratch/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.linuxnme.tffromscratch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
 
 public class MainActivity extends AppCompatActivity {
 
